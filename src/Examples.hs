ae77f9997e9901e0663100fec635557d4afb0b25
Oopscurity <headlessfirst@gmail.com>
2016-09-29 17:23:31 +0300

    atan': more similar Taylor series representation

--- src/Examples.hs
+++ src/Examples.hs
@@ -9,14 +9,14 @@ atan' x
   | x < 0 = -atan' (-x)
   | x > 1 = pi / 2 - atan' (1 / x)
   | otherwise =
-      iterate x 1 -- Teylor series for atan
+      iterate x 0 -- Taylor series for atan
     where
       n' = 30
       iterate :: Double -> Int -> Double
       iterate x' n
           | n == n' = 0
           | otherwise =
-              (-1) ^ (n - 1) * x' / fromIntegral (2 * n - 1)
+              (-1) ^ n * x' / fromIntegral (2 * n + 1)
               + iterate (x' * x * x) (n + 1)
 
 -- NOTE: E(acot'x) = (0; pi)
