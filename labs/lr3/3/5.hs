6befe953ab53ad80cdb77c64bed982b407ae827d
Oopscurity <headlessfirst@gmail.com>
2016-11-22 23:49:12 +0300

    Implement fib2 with linear time complexity

--- labs/lr3/3/5.hs
+++ labs/lr3/3/5.hs
@@ -8,13 +8,20 @@
         {a(k) = a(k - 1) + a(k - 2), k > 1.
   -}
 
-  fib1 :: (Ord a, Integral a, Integral b) => a -> b
+  fib1 :: (Integral a, Integral b) => a -> b
   fib1 1 = 1
   fib1 2 = 1
   fib1 k = fib1 (k - 1) + fib1 (k - 2)
 
-  --fib2 1 = 1
-  --fib2 2 = 1
-  --fib2 n = iterate n 1 1
-  --  where
-  --    iterate acc l ll = 
+  fib2 :: (Integral a, Integral b) => a -> b
+  fib2 1 = 1
+  fib2 2 = 1
+  fib2 k = it 3 (fib2 2) (fib2 1)
+    where
+      it i p pp | i == k = current
+                | True   = it (i + 1) current p
+                where current = p + pp
+
+  -- TODO: infinite fib list
+
+  -- Неудачные тестовые примеры:
